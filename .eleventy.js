ed303fdb754be5a65571732794d052ea512d2cb2
Diego Montivero <diego.montivero91@gmail.com>
2022-10-14 20:08:29 +0200

    Added SASS

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+const eleventySass = require("eleventy-sass");
+
 module.exports = function (eleventyConfig) {
+  // SASS
+  eleventyConfig.addPlugin(eleventySass);
+
   // Set custom input and output dir
   return {
     dir: {
